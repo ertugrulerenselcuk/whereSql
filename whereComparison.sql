93b1f35ecf90cb11c8b76a0fdf556631275a17cb
Ertugrul Eren Selcuk <83607933+ertugrulerenselcuk@users.noreply.github.com>
2024-12-07 21:58:07 +0300

    whereComparison.sql

---
+++ whereComparison.sql
@@ -0,0 +1,11 @@
+SELECT * FROM actor
+WHERE first_name = 'Penelope'
+
+SELECT * from customer
+WHERE store_id = 1;
+
+SELECT * FROM payment
+WHERE staff_id <> 2;
+
+SELECT * FROM film
+WHERE length >= 100
